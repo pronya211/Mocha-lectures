80f502ab258c2ee86821fa8f2ac95ee86f95b2c3
pronya211 <211poster@gmail.com>
2023-10-15 23:06:06 +0300

    added homework

---
+++ cypress/e2e/homeWork.cy.js
@@ -0,0 +1,49 @@
+describe('multiple search tests', () => {
+
+  const searchTerms = [
+    {
+      testData: {
+        email: "testuser1@gmail.com",
+        password: "testpassword1"
+      }
+    },
+    {
+      testData: {
+        email: "testuser2@gmail.com",
+        password: "testpassword2"
+      }
+    },
+    {
+      testData: {
+        email: "testuser3@gmail.com",
+        password: "testpassword3"
+      }
+    },
+    {
+      testData: {
+        email: "testuser4@gmail.com",
+        password: "testpassword4"
+      }
+    },
+    {
+      testData: {
+        email: "testuser5@gmail.com",
+        password: "testpassword5"
+      }
+    }
+  ]
+
+  searchTerms.forEach(searchTerm => {
+    it(`Serch term: ${searchTerm.testData}`, () => {
+      cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+
+      cy.get('[alt="Material Dark Theme"]').click();
+      cy.get('.menu-title.ng-tns-c141-9').click();
+      cy.get('.menu-title.ng-tns-c141-11').click();
+      cy.get('[id="inputEmail1"]').type(searchTerm.testData.email);
+      cy.get('[id="inputPassword2"]').type(searchTerm.testData.password);
+    })
+  })
+
+})
+
